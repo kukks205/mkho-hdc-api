7ba05a42ada7b53bbd91e74d9b4d040901c535ff
Satit Rianpit <rianpit@gmail.com>
2017-04-21 10:28:08 +0700

    export anc target

--- src/models/anc.ts
+++ src/models/anc.ts
@@ -48,4 +48,29 @@ export class AncModel {
       });
     });
   }
+
+  getAncTarget(connection: IConnection, hospcode: string) {
+    return new Promise((resolve, reject) => {
+      let sql = `
+        select p.HOSPCODE as hospcode, p.PID as pid, p.CID as cid, concat(p.NAME, " ", p.LNAME) as ptname, 
+        date_format(p.BIRTH, '%Y-%m-%d') as birth, TIMESTAMPDIFF(year, p.BIRTH, current_date()) as age, p.SEX as sex,
+        t.gravida, t.bdate, t.bhosp, t.input_bhosp,
+        t.g1_ga, date_format(t.g1_date, '%Y-%m-%d') as g1_date, t.g1_hospcode, t.g1_input_hosp,
+        t.g2_ga, date_format(t.g2_date, '%Y-%m-%d') as g2_date, t.g2_hospcode, t.g2_input_hosp,
+        t.g3_ga, date_format(t.g3_date, '%Y-%m-%d') as g3_date, t.g3_hospcode, t.g3_input_hosp,
+        t.g4_ga, date_format(t.g4_date, '%Y-%m-%d') as g4_date, t.g4_hospcode, t.g4_input_hosp,
+        t.g5_ga, date_format(t.g5_date, '%Y-%m-%d') as g5_date, t.g5_hospcode, t.g5_input_hosp
+        from t_person_anc as t
+        inner join person as p on p.HOSPCODE=t.hospcode and p.PID=t.pid
+        where t.hospcode = ?
+        and t.typearea in ('1', '3')
+        group by t.pid, t.gravida
+      `;
+      // run query
+      connection.query(sql, [hospcode], (err, results) => {
+        if (err) reject(err);
+        else resolve(results);
+      });
+    });
+  }
 }
--- src/routes/anc.ts
+++ src/routes/anc.ts
@@ -3,8 +3,14 @@
 import * as express from 'express';
 import { IConnection } from 'mysql';
 
+import * as moment from 'moment';
+import * as path from 'path';
+
 import { Connection } from '../models/connection';
 import { AncModel } from '../models/anc';
+
+const json2xls = require('json2xls');
+const fse = require('fs-extra');
 
 const connection = new Connection();
 const ancModel = new AncModel();
@@ -58,5 +64,45 @@ router.post('/post/history',(req,res,next) => {
     res.send({ok: false, message: 'กรุณาระบุเลขบัตรประชาชน'})
   }
 });
+
+
+
+router.get('/excel-export', (req, res, next) => {
+  const hospcode = req.query.hospcode;
+  console.log(hospcode);
+  
+  if (hospcode) {
+    connection.getConnection()
+      .then((conn: IConnection) => {
+        _conn = conn;
+        return ancModel.getAncTarget(_conn, hospcode);
+      })
+      .then((result: any) => {
+        _conn.destroy();
+        //res.send({ ok: true, rows: result });
+        let xcel = json2xls(result);
+        let tmpDir = process.env.TMP_FOLDER;
+        fse.ensureDirSync(tmpDir);
+        let _f = `anc-${hospcode}-${moment().format('x')}.xls`;
+        let fileName = path.join(tmpDir, _f);
+        fse.writeFileSync(fileName, xcel, 'binary');
+        res.download(fileName, (err) => {
+          if (err) {
+            res.send({ ok: false, message: err })
+          } else {
+            fse.removeSync(fileName);
+          }
+        });
+      })
+      .catch(error => {
+        _conn.destroy();
+        res.send({ ok: false, message: error });
+      });
+  } else {
+    res.send({ ok: false, message: 'ข้อมูลไม่สมบูรณ์' })
+  }
+
+});
+
 
 export default router;
